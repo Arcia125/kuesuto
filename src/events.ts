310d286b8e809b1a351ccd2a3b85c73dbecc5820
Kevin Hallett <kevinmhallett@gmail.com>
2023-10-17 12:04:50 -0500

    refactor EventEmitter

--- src/events.ts
+++ src/events.ts
@@ -2,38 +2,37 @@ type EventListener<T = any> = (eventName: string, payload?: T) => void;
 
 class EventEmitter {
   public static ALL = 'all';
-  private events: Record<string, EventListener[]> = {
+  private listeners: Record<string, EventListener[]> = {
     [EventEmitter.ALL]: [],
   };
 
   on<T>(eventName: string, listener: EventListener<T>) {
-    // const isAll = eventName === 'all';
-    if (!this.events[eventName]) {
-      this.events[eventName] = [];
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
     }
 
-    this.events[eventName].push(listener);
+    this.listeners[eventName].push(listener);
   }
 
 
   emit<T>(eventName: string, payload?: T) {
 
-    const listeners = this.events[eventName];
+    const listeners = this.listeners[eventName];
 
     if (listeners) {
       listeners.forEach(listener => listener(eventName, payload));
     }
-    const allListeners = this.events[EventEmitter.ALL];
+    const allListeners = this.listeners[EventEmitter.ALL];
     if (allListeners) {
       allListeners.forEach(listener => listener(eventName, payload));
     }
   }
 
   off<T>(eventName: string, listener: EventListener<T>) {
-    const listeners = this.events[eventName];
+    const listeners = this.listeners[eventName];
 
     if (listeners) {
-      this.events[eventName] = listeners.filter(l => l !== listener);
+      this.listeners[eventName] = listeners.filter(l => l !== listener);
     }
   }
 }
