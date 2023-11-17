c01a8d2dbf7882c2bf2aa321a86889ef4ea9c12a
Kevin Hallett <kevinmhallett@gmail.com>
2023-11-16 22:55:27 -0600

    fix errors and build

--- src/systems/physicsSystem.ts
+++ src/systems/physicsSystem.ts
@@ -1,7 +1,6 @@
 import { Collision } from '../capabilities/collision';
-import { WeaponEntity } from '../entities/weaponEntity';
 import { EventEmitter, EVENTS, EVENT_MAPPING } from '../events';
-import { Damage, Force, ForceEntry, GameEntity, GameState, IPhysicsSystem } from '../models';
+import { Force, ForceEntry, GameEntity, GameState, IPhysicsSystem } from '../models';
 import { directionVectorBetween } from '../position';
 import { getSpriteScale } from '../sprites';
 
@@ -9,52 +8,25 @@ import { getSpriteScale } from '../sprites';
 export class PhysicsSystem implements IPhysicsSystem {
   private forceEntries: ForceEntry[] = [];
 
-  // public static getDamages(entity: GameEntity): Damage[] {
-  //   // console.warn('getDamage not implemented');
-  //   const weapon = entity.children?.find(child => child instanceof WeaponEntity) as WeaponEntity | undefined;
-  //   if (!weapon) {
-  //     console.error(`getDamages: cannot find weapon on entity ${entity}`);
-  //     return [];
-  //   }
-  //   const damages = weapon.stats.damages;
-  //   return damages;
-  // }
-
-  public constructor(private emitter: EventEmitter) {
+  public constructor(emitter: EventEmitter) {
     emitter.on(EVENTS.ATTACK, (_eventName, payload) => this.handleAttack(payload))
   }
 
   private handleAttack = ({ attacker, target }: EVENT_MAPPING[typeof EVENTS.ATTACK]) => {
-    // const damages = DamageSystem.getDamages(attacker);
-    // this.dealDamage(attacker, damages, target);
     this.applyForce(target, {
       direction: directionVectorBetween(attacker.state, target.state),
       magnitude: 1000
     });
   };
 
-
   private registerForce = (forceEntry: ForceEntry) => this.forceEntries.push(forceEntry);
 
   public applyForce = (entity: GameEntity, force: Force) => {
-    if (entity.status.immortal) {
-      return;
-    }
-
+    if (entity.status.immortal) return;
     this.registerForce({ entity, force });
-
-
-    // this.applyForce
-
-
-    // damages.forEach(damage => {
-    //   entity.status.health -= damage.power;
-    // });
-
-    // this.emitter.emit(EVENTS.DAMAGE, { attacker, entity, damages });
   }
 
-  public update = (gameState: GameState, timestamp: number) => {
+  public update = (gameState: GameState, _timestamp: number) => {
     for (let i = 0; i < this.forceEntries.length; i++) {
       const forceEntry = this.forceEntries[i];
       const { entity, force } = forceEntry;
@@ -89,41 +61,3 @@ export class PhysicsSystem implements IPhysicsSystem {
     }
   }
 }
-
-// import { WeaponEntity } from '../entities/weaponEntity';
-// import { EventEmitter, EVENTS, EVENT_MAPPING } from '../events';
-// import { Damage, GameEntity, IDamageSystem } from '../models';
-
-// export class DamageSystem implements IDamageSystem {
-
-//   public static getDamages(entity: GameEntity): Damage[] {
-//     // console.warn('getDamage not implemented');
-//     const weapon = entity.children?.find(child => child instanceof WeaponEntity) as WeaponEntity | undefined;
-//     if (!weapon) {
-//       console.error(`getDamages: cannot find weapon on entity ${entity}`);
-//       return [];
-//     }
-//     const damages = weapon.stats.damages;
-//     return damages;
-//   }
-
-//   public constructor(private emitter: EventEmitter) {
-//     emitter.on(EVENTS.ATTACK, (_eventName, payload) => this.handleAttack(payload))
-//   }
-
-//   public handleAttack = ({ attacker, target }: EVENT_MAPPING[typeof EVENTS.ATTACK]) => {
-//     const damages = DamageSystem.getDamages(attacker);
-//     this.dealDamage(attacker, damages, target);
-//   };
-
-//   public dealDamage = (attacker: GameEntity, damages: Damage[], target: GameEntity) => {
-//     if (target.status.immortal) {
-//       return;
-//     }
-//     damages.forEach(damage => {
-//       target.status.health -= damage.power;
-//     });
-
-//     this.emitter.emit(EVENTS.DAMAGE, { attacker, target, damages });
-//   }
-// }
